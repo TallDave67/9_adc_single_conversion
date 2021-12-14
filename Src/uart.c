2108c5a4db60823891d005fc7ac2f57617e552a5
David Bellis <david.bellis1967@gmail.com>
2021-12-14 11:29:48 +0100

    Update uart.c

--- Src/uart.c
+++ Src/uart.c
@@ -12,7 +12,7 @@
 #define SYS_FREQ		16000000
 #define APB1_CLK		SYS_FREQ
 
-#define UART_BAUDRATE	115200
+#define UART_BAUDRATE           115200
 
 static void uart_set_baudrate(USART_TypeDef *USARTx, uint32_t PeriphClk, uint32_t BaudRate);
 static uint16_t compute_uart_bd(uint32_t PeriphClk, uint32_t BaudRate);
